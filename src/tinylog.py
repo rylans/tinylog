aaa127adfb0bb88e95283025069781b50a61480b
Rylan <rylans@gmail.com>
2014-04-29 12:08:51 -0700

    Fixed a bug in setLevel.

--- src/tinylog.py
+++ src/tinylog.py
@@ -11,9 +11,8 @@ class tinylog:
   def __init__(self, filename, level=ALL):
     self.filename = open(filename, "a+")
     self.level = level
-    print "logging level is " + str(level)
 
-  def setLevel(level):
+  def setLevel(self, level):
     self.level = level
 
   def write(self, levelstring, lvl, msg):
@@ -21,8 +20,6 @@ class tinylog:
       t = str(datetime.datetime.utcnow()).split('.')[0]
       output_message = t + " " + levelstring + " " + msg
       self.filename.write(output_message + "\n")
-    else:
-      print "message ignored"
 
   def debug(self, msg):
     self.write("DEBUG", 1,  msg)
@@ -40,16 +37,9 @@ class tinylog:
     self.write("CRITICAL", 5, msg)
 
 if __name__ == '__main__':
-  log = tinylog("out.log")
-  log.info("Here is an info message")
-  log.warn("There's a problem")
-  log.debug("This is a debug message")
-  log.error("An error is happening")
-  log.critical("Something bad is happening")
-
-  log = tinylog("out2.log", ERROR)
-  log.info("Here is an info message")
-  log.warn("There's a problem")
-  log.debug("This is a debug message")
-  log.error("An error is happening")
-  log.critical("Something bad is happening")
+  log = tinylog("out.log", ALL)
+  log.info("Info")
+  log.warn("Warning")
+  log.debug("Debug")
+  log.error("Error")
+  log.critical("Critical")
